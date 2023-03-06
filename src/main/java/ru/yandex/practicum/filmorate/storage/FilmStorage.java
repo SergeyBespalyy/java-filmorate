94a19fe9a461d41b775fc698e6bee88a3f8af2c5
Sergey Bespalyy <bespaliy.sergei@yandex.ru>
2023-03-06 10:43:09 +0300

    feature: добавить интерфейс UserStorage и FilmStorage

---
+++ src/main/java/ru/yandex/practicum/filmorate/storage/FilmStorage.java
@@ -0,0 +1,24 @@
+package ru.yandex.practicum.filmorate.storage;
+
+import ru.yandex.practicum.filmorate.model.Film;
+
+import java.util.Collection;
+
+/**
+ * Интерфейс FilmStorage имеющий
+ * методы добавления, удаления и модификации объектов
+ */
+
+public interface FilmStorage {
+
+    Collection<Film> findAllFilm();
+
+    Film findFilmById(Integer filmId);
+
+    Film addFilm(Film film);
+
+    void deleteFilm(Film film);
+
+    Film updateFilm(Film film);
+
+}
